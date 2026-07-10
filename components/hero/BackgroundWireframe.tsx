e98c915316b5e674ec482b750ae6347b47904461
lukarud <blackgomun@gmail.com>
2026-07-11 00:06:07 +0200

    Refine hero background linework

--- components/hero/BackgroundWireframe.tsx
+++ components/hero/BackgroundWireframe.tsx
@@ -1,9 +1,25 @@
-function Line({ className }: { className: string }) {
-  return <div className={`rounded-full border border-[#dedfe3] ${className}`} />;
+function TraceLine({ className }: { className: string }) {
+  return (
+    <div
+      className={`absolute h-px bg-gradient-to-r from-transparent via-[#c9cbd3]/80 to-transparent ${className}`}
+    />
+  );
 }
 
-function DashedBox({ className }: { className: string }) {
-  return <div className={`rounded-[28px] border border-dashed border-[#dedfe3] ${className}`} />;
+function TraceNode({ className }: { className: string }) {
+  return (
+    <div
+      className={`absolute size-2 rounded-full border border-[#c6c8d0] bg-[#f4f4f6] shadow-[0_0_0_5px_rgba(244,244,246,0.86)] ${className}`}
+    />
+  );
+}
+
+function SoftPanel({ className }: { className: string }) {
+  return (
+    <div
+      className={`absolute rounded-[28px] border border-[#dfe0e6]/80 bg-white/[0.16] shadow-[inset_0_1px_0_rgba(255,255,255,0.7)] ${className}`}
+    />
+  );
 }
 
 export default function BackgroundWireframe() {
@@ -12,50 +28,52 @@ export default function BackgroundWireframe() {
       className="pointer-events-none absolute inset-x-0 top-[104px] z-0 hidden h-[760px] select-none overflow-hidden px-7 xl:block"
       aria-hidden="true"
     >
-      <div className="relative mx-auto grid h-full max-w-[1860px] grid-cols-[365px_minmax(760px,1fr)_365px] gap-4 opacity-95">
-        <div className="relative h-full rounded-[34px] border border-[#dedfe3] bg-transparent">
-          <div className="absolute inset-[10px] rounded-[28px] border border-dashed border-[#dedfe3]" />
-          <div className="absolute left-1/2 top-[28px] h-[126px] w-[126px] -translate-x-1/2 rounded-full border border-[#dedfe3]" />
-          <Line className="absolute left-[28px] right-[28px] top-[168px] h-[32px]" />
-          <Line className="absolute left-[74px] right-[74px] top-[212px] h-[32px]" />
-          <DashedBox className="absolute left-[28px] right-[28px] top-[278px] h-[236px]" />
-          <Line className="absolute bottom-[112px] left-[28px] h-[32px] w-[156px]" />
-          <Line className="absolute bottom-[112px] left-[194px] h-[32px] w-[68px]" />
-          <Line className="absolute bottom-[112px] right-[28px] h-[32px] w-[66px]" />
-          <Line className="absolute bottom-[70px] left-[28px] h-[32px] w-[84px]" />
-          <Line className="absolute bottom-[70px] left-[124px] right-[28px] h-[32px]" />
-          <Line className="absolute bottom-[28px] left-[28px] right-[28px] h-[32px]" />
-        </div>
+      <div className="relative mx-auto h-full max-w-[1860px] opacity-80">
+        <div className="absolute inset-x-[390px] top-0 h-full rounded-[34px] border border-[#dedfe3]/80 bg-[linear-gradient(180deg,rgba(255,255,255,0.18),rgba(255,255,255,0))]" />
+        <div className="absolute inset-x-[400px] top-[10px] h-[calc(100%-20px)] rounded-[28px] border border-[#ececf1]" />
 
-        <div className="relative h-full rounded-[34px] border border-[#dedfe3] bg-transparent">
-          <div className="absolute inset-[10px] rounded-[28px] border border-dashed border-[#dedfe3]" />
-        </div>
+        <div className="absolute inset-x-[420px] top-[88px] h-px bg-gradient-to-r from-transparent via-[#d4d5dc] to-transparent" />
+        <div className="absolute inset-x-[460px] bottom-[88px] h-px bg-gradient-to-r from-transparent via-[#d4d5dc] to-transparent" />
 
-        <div className="grid h-full grid-rows-[320px_1fr] gap-4">
-          <div className="relative rounded-[34px] border border-[#dedfe3]">
-            <div className="absolute inset-[10px] rounded-[28px] border border-dashed border-[#dedfe3]" />
-            {[0, 1, 2, 3].map((index) => (
-              <div
-                key={index}
-                className="absolute left-[28px] h-[42px] w-[42px] rounded-full border border-[#dedfe3]"
-                style={{ top: 48 + index * 60 }}
-              />
-            ))}
-            <Line className="absolute left-[88px] right-[28px] top-[48px] h-[42px]" />
-            <DashedBox className="absolute left-[88px] right-[28px] top-[108px] h-[42px] rounded-[24px]" />
-            <DashedBox className="absolute left-[88px] right-[28px] top-[168px] h-[42px] rounded-[24px]" />
-            <DashedBox className="absolute left-[88px] right-[28px] top-[228px] h-[42px] rounded-[24px]" />
-          </div>
+        <TraceLine className="left-[98px] top-[182px] w-[310px]" />
+        <TraceLine className="left-[160px] top-[332px] w-[220px]" />
+        <TraceLine className="left-[92px] bottom-[150px] w-[300px]" />
+        <TraceLine className="right-[104px] top-[204px] w-[300px]" />
+        <TraceLine className="right-[148px] top-[386px] w-[230px]" />
+        <TraceLine className="right-[82px] bottom-[176px] w-[310px]" />
 
-          <div className="relative rounded-[34px] border border-[#dedfe3]">
-            <div className="absolute inset-[10px] rounded-[28px] border border-dashed border-[#dedfe3]" />
-            <Line className="absolute left-[28px] right-[28px] top-[28px] h-[220px] rounded-[28px]" />
-            <Line className="absolute bottom-[124px] left-[28px] h-[82px] w-[148px] rounded-[28px]" />
-            <Line className="absolute bottom-[124px] right-[28px] h-[82px] w-[148px] rounded-[28px]" />
-            <Line className="absolute bottom-[28px] left-[28px] h-[82px] w-[148px] rounded-[28px]" />
-            <Line className="absolute bottom-[28px] right-[28px] h-[82px] w-[148px] rounded-[28px]" />
-          </div>
-        </div>
+        <div className="absolute left-[70px] top-[112px] h-[540px] w-[300px] rounded-[30px] border border-[#e0e1e7]/70 bg-white/[0.08]" />
+        <div className="absolute right-[70px] top-[118px] h-[520px] w-[300px] rounded-[30px] border border-[#e0e1e7]/70 bg-white/[0.08]" />
+
+        <SoftPanel className="left-[110px] top-[154px] h-[78px] w-[160px]" />
+        <SoftPanel className="left-[128px] top-[274px] h-[118px] w-[205px]" />
+        <SoftPanel className="left-[96px] bottom-[120px] h-[74px] w-[230px]" />
+
+        <SoftPanel className="right-[106px] top-[156px] h-[92px] w-[220px]" />
+        <SoftPanel className="right-[148px] top-[316px] h-[126px] w-[170px]" />
+        <SoftPanel className="right-[92px] bottom-[118px] h-[78px] w-[238px]" />
+
+        <div className="absolute left-[202px] top-[180px] h-[1px] w-[170px] rotate-[26deg] bg-gradient-to-r from-[#d6d7de] to-transparent" />
+        <div className="absolute left-[234px] bottom-[206px] h-[1px] w-[150px] -rotate-[18deg] bg-gradient-to-r from-[#d6d7de] to-transparent" />
+        <div className="absolute right-[228px] top-[250px] h-[1px] w-[160px] -rotate-[22deg] bg-gradient-to-l from-[#d6d7de] to-transparent" />
+        <div className="absolute right-[240px] bottom-[212px] h-[1px] w-[160px] rotate-[20deg] bg-gradient-to-l from-[#d6d7de] to-transparent" />
+
+        <TraceNode className="left-[96px] top-[178px]" />
+        <TraceNode className="left-[366px] top-[178px]" />
+        <TraceNode className="left-[156px] top-[328px]" />
+        <TraceNode className="left-[374px] top-[328px]" />
+        <TraceNode className="left-[90px] bottom-[146px]" />
+        <TraceNode className="left-[386px] bottom-[146px]" />
+        <TraceNode className="right-[96px] top-[200px]" />
+        <TraceNode className="right-[398px] top-[200px]" />
+        <TraceNode className="right-[144px] top-[382px]" />
+        <TraceNode className="right-[382px] top-[382px]" />
+        <TraceNode className="right-[78px] bottom-[172px]" />
+        <TraceNode className="right-[392px] bottom-[172px]" />
+
+        <div className="absolute left-[402px] top-[52px] h-[656px] w-px bg-gradient-to-b from-transparent via-[#e1e2e8] to-transparent" />
+        <div className="absolute right-[402px] top-[52px] h-[656px] w-px bg-gradient-to-b from-transparent via-[#e1e2e8] to-transparent" />
+        <div className="absolute left-[410px] right-[410px] top-[10px] h-[220px] bg-[radial-gradient(ellipse_at_50%_0%,rgba(115,87,255,0.10),transparent_68%)]" />
       </div>
     </div>
   );
